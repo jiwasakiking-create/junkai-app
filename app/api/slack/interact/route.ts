065da16e4e273117ac8f678fe011971668a6899d
iwasaki joichiro <j.iwasaki.king@gmail.com>
2026-02-17 14:26:24 +0900

    Add Slack buttons and update to 5 members

--- app/api/slack/interact/route.ts
+++ app/api/slack/interact/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { createClient } from '../../../lib/supabase'
+import { createClient } from '../../../../lib/supabase'
 
 export async function POST(request: Request) {
   const formData = await request.formData()
